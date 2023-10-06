ce592dc7326eca3ca3650f3325d3e85c564f99de
abdulmohaimen0 <likeadragon111@gmail.com>
2023-10-06 12:08:41 +0400

    set debug to false

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import easyocr
 import cv2
 import re
 import numpy as np
+import os
 
 
 app = Flask(__name__)
@@ -81,4 +82,5 @@ def ocr():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = os.environ.get("PORT", 5000)
+    app.run(debug=False, host="0.0.0.0", port=port)
